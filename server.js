c38ca0a15df530edcc760fd913e2fcc5965e8c12
Skylar Harwell <swharwell@gmail.com>
2021-10-02 21:54:41 -0400

    Folder structure addition

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,12 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({  });
 
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
+  
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
@@ -35,6 +36,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+app.get('/', (req,res) => {
+  res.send("Hello World")
+})
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
